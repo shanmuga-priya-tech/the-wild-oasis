d1891b66ea115065c6799cfb67bba2b11a3ee1dc
shany-shan <priyaeswaran2023@gmail.com>
2024-05-25 22:16:17 +0530

    implemented a graph using recharts library with fake data

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import { Stats } from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -31,7 +32,7 @@ function DashboardLayout() {
 
       <div>Today activity</div>
       <div>chart stay duration</div>
-      <div>chart sales</div>
+      <SalesChart />
     </StyledDashboardLayout>
   );
 }
