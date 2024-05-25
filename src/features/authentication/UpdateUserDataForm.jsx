a0154f056135fb26140cb82cfb60d3257caee438
shany-shan <priyaeswaran2023@gmail.com>
2024-05-25 11:18:57 +0530

    created a static version and an api to update user info

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -6,7 +6,7 @@ import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
-import { useUser } from "./useUser";
+import useUser from "./useUser";
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
